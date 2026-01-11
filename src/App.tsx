d62e3f491790d0a2f77f1911bc4b6b056f9fbbfc
clarissaalbarr <cmalbarracin15@gmail.com>
2026-01-11 23:32:18 +0800

    Install Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -27,6 +29,9 @@ function App() {
       <p className="read-the-docs text-amber-300">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </>
   );
 }
