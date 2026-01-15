d38dac525028ca495f6b0e11bdd547430c44119f
clarissaalbarr <cmalbarracin15@gmail.com>
2026-01-15 12:55:23 +0800

    Finished main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import NavBar from "@/components/NavBar";
+import MobileNavBar from "./components/MobileNavBar";
+import MainSection from "@/components/MainSection";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <NavBar></NavBar>
+      <MobileNavBar />
+      <div className="gap-3 flex flex-col">
+        <MainSection />
+      </div>
     </>
   );
 }
---
+++ src/components/MainSection.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import { Mail, File } from "lucide-react";
+import resumePDF from "@/assets/docs/Albarracin, Clarissa - Resume.pdf";
+
+export default function MainSection() {
+  const EMAIL_ADDRESS = "clarissa.albarracin@gmail.com";
+  const DOWNLOAD_FILENAME = "ALBARRACIN_CLARISSA-Resume.pdf";
+
+  const MAILTO_URL = `mailto:${EMAIL_ADDRESS}`;
+
+  return (
+    <div className="flex flex-col items-center px-8 py-14 gap-6 ">
+      <section id="main" className="flex flex-col gap-2 items-start">
+        <div className="text-2xl gap-2 flex">
+          <span>Hi!</span>
+          <span>👋</span>
+        </div>
+        <div className="text-2xl">
+          I'm <span className="font-bold">Clarissa Albarracin</span>,
+        </div>
+        <p>
+          an aspiring front-end developer bridging design and development to
+          build polished, intuitive web experiences.
+        </p>
+      </section>
+
+      <div className="flex gap-3">
+        <a href={resumePDF} download>
+          <Button variant="outline" size="sm">
+            <File /> Download Resume
+          </Button>
+        </a>
+        <a href={MAILTO_URL}>
+          <Button variant="default" size="sm">
+            <Mail /> Get in touch
+          </Button>
+        </a>
+      </div>
+    </div>
+  );
+}
